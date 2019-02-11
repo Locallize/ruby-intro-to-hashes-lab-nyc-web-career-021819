2152db496b553b2a334730afc0faa6d67bb4f1ac
Fillan Lee <fillanlee@aol.com>
2019-02-11 02:54:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,9 @@ def monopoly_with_third_tier
 	          :b_and_o_railroad =>{},
 	          :shortline => {}},
 	:rent_in_dollars => {:one_piece_owned => 25,  
-	                    :two_piece_owned =>50,
-	                    :three_piece_owned =>100,
-	                    :four_piece_owned => 200 }}}
+	                    :two_pieces_owned => 50,
+	                    :three_pieces_owned =>100,
+	                    :four_pieces_owned => 200 }}}
 	
 
 end
@@ -36,8 +36,8 @@ def monopoly_with_fourth_tier
 	:b_and_o_railroad =>{:mortgage_value => "$400"},
 	:shortline =>{:mortgage_value => "$800"}},
 	:rent_in_dollars => {:one_piece_owned => 25,  
-	                    :two_piece_owned =>50,
-	                    :three_piece_owned =>100,
-	                    :four_piece_owned => 200 }}}
+	                    :two_pieces_owned =>50,
+	                    :three_pieces_owned =>100,
+	                    :four_pieces_owned => 200 }}}
 
 end
