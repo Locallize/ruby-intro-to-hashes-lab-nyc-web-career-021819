0345c3425fb823c65d2b98400a7c889f0e52746e
Fillan Lee <fillanlee@aol.com>
2019-02-11 02:52:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	:railroads => {:pieces => 4,:names =>{},:rent_in_dollar => {}}}
+	:railroads => {:pieces => 4,:names =>{},:rent_in_dollars => {}}}
 end
 
 def monopoly_with_third_tier
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
 	          :pennsylvania_railroad =>{}, 
 	          :b_and_o_railroad =>{},
 	          :shortline => {}},
-	:rent_in_dollar => {:one_piece_owned => 25,  
+	:rent_in_dollars => {:one_piece_owned => 25,  
 	                    :two_piece_owned =>50,
 	                    :three_piece_owned =>100,
 	                    :four_piece_owned => 200 }}}
