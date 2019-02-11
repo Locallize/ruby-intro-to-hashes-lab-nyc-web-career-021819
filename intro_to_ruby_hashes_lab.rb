51caad8fdefc0e54d0cb2d518f56fc633ea8183a
Fillan Lee <fillanlee@aol.com>
2019-02-11 02:36:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,6 @@ def monopoly_with_fourth_tier
 	:Rent_in_dollars => {:one_piece_owned => 25,  
 	                    :two_piece_owned =>50,
 	                    :three_piece_owned =>100,
-	                    :four_piece_owned => 200 },
-	}
+	                    :four_piece_owned => 200 }}
 
 end
